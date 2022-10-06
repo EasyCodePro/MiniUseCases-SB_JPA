f107e35ecdd4290f142dc1e7b13108f9e8c0bb36
prosenjit005 <115132481+EasyCodePro@users.noreply.github.com>
2022-10-06 17:03:44 +0530

    initial commit

---
+++ JPA_Queries_1/JPA_Queries_1/src/main/java/com/sbjpa/jpaquery1/JpaQueries1Application.java
@@ -0,0 +1,13 @@
+package com.sbjpa.jpaquery1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaQueries1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaQueries1Application.class, args);
+	}
+
+}
---
+++ JPA_Queries_1/JPA_Queries_1/src/main/java/com/sbjpa/jpaquery1/controller/JpaQuery1Controller.java
@@ -0,0 +1,8 @@
+package com.sbjpa.jpaquery1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JpaQuery1Controller {
+
+}
